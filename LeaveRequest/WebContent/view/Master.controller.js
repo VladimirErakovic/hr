770dd378086de003632b1271c5388c4f0e701326
Vladimir Erakovic <vladimirerakovic@gmail.com>
2018-06-05 10:00:11 +0200

    Update Master.controller.js

--- LeaveRequest/WebContent/view/Master.controller.js
+++ LeaveRequest/WebContent/view/Master.controller.js
@@ -22,14 +22,14 @@ sap.ui.define([
 				oJQueryStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local);					
 				userPernr = oJQueryStorage.get("user_pernr");
 							
-		        var oBundle = jQuery.sap.resources({url: "i18n/i18n.properties"});
+		        	var oBundle = jQuery.sap.resources({url: "i18n/i18n.properties"});
 				
 				user = oBundle.getText("GatewayUsername");
 				pass = oBundle.getText("GatewayPassword");
 					
 				oView = this.getView().byId("historyList");	
 				
-				var url = "proxy/https/sapgw.mk-group.org:42080/sap/opu/odata/SAP/ZHR_GET_LR_HISTORY_SRV/";
+				var url = "proxy/https/***/sap/opu/odata/SAP/ZHR_GET_LR_HISTORY_SRV/";
 				var oModelHistory = new sap.ui.model.odata.ODataModel(url, false, user, pass);
 				sap.ui.getCore().setModel(oModelHistory, "oModelHistory"); // for refreshing model from details
 				oView.setModel(oModelHistory);
